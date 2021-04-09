56bd2729d19d7ecb29c79fa39d8eee594463822e
lillianshern <72427064+lillianshern@users.noreply.github.com>
2021-04-09 12:05:50 +0800

    music

--- challengeISG/challengeISG.js
+++ challengeISG/challengeISG.js
@@ -1,27 +1,28 @@
-var sadend = new Audio("https://youtu.be/7PtIHBCuR-Q")
-var happyend = new Audio("https://youtu.be/pjDFB5_WfOE")
+var sadend = new Audio("IB.mp3")
+var happyend = new Audio("SA.mp3")
+var slowTimer
+
+function runTimer (){
+    slowTimer = setTimeout(tooSlow, 8000)
+}
 
 function gameStart() {
     // Start timer
     runTimer()
     // hide the start button 
     document.querySelector("#start").style.display = "none"
-
+    
     // Display a prompt box which ask the user for her/his name, and output a message:
     var player = prompt("Please enter your name", "Senior");
-  
+    
     if (player != null) {
         document.getElementById("p1").innerHTML =
         "Hi, " + player + ", you're a senior in PAS. It is May, and you're starting to prepare for college applications. <br><br> Pamela: " + "So, " +
         player + ", what's your intended major?"
     }
-
-    // if the player didnt choose the options within 10 seconds
-    var slowTimer
     
-    function runTimer (){
-        slowTimer = setTimeout(tooSlow, 8000)
-    }
+    // if the player didnt choose the options within 10 seconds
+    
 
     document.querySelector("#one").innerText = "Sociology"
     document.querySelector("#one").style.display = "block"
@@ -68,11 +69,6 @@ function gameStart() {
     document.querySelector(".container").style.backgroundImage = "URL('end-salaryman.gif')"
     document.querySelector("#one").style.display = "none"
     document.getElementById("p1").innerHTML = "Pamela: *Looks at your transcript* Hmm... YOUR GPA DOES NOT EVEN QUALIFY you to GRADUATE, YOU'LL never get into any universities' department with that GPA. You failed her expectation, after highschool, you became a normal salaryman working from 9am to 10pm."
-
-
-    document.querySelector("#two").innerText = "Restart"
-    document.querySelector("#two").style.display = "block"
-    document.querySelector("#two").onclick= gameStart
  }
  
 function deadend() {
