3020bbfd5b23d631a1f6b077d04275c99dcf14b5
lillianshern <72427064+lillianshern@users.noreply.github.com>
2021-04-12 04:15:19 +0800

    finish

--- challengeISG/challengeISG.js
+++ challengeISG/challengeISG.js
@@ -24,17 +24,17 @@ function gameStart() {
         "<br><br> Pamela: " + "So, " + player + ", what's your intended major?"
     }
 
-    document.querySelector("#one").innerText = "Sociology"
-    document.querySelector("#one").style.display = "block"
+    document.querySelector("#p").innerText = "Sociology"
+    document.querySelector("#p").style.display = "block"
 
-    document.querySelector("#two").innerText = "Business"
-    document.querySelector("#two").style.display = "block"
+    document.querySelector("#onep").innerText = "Business"
+    document.querySelector("#onep").style.display = "block"
 }
   
 function tooSlow() {
     sadend.play()
-    document.querySelector("#one").style.display = "none"
-    document.querySelector("#two").style.display = "none"
+    document.querySelector("#p").style.display = "none"
+    document.querySelector("#onep").style.display = "none"
 
     document.querySelector(".container").style.backgroundImage = "URL('end-homeless.gif')"
     
@@ -61,36 +61,38 @@ function res() {
 
     document.getElementById("p1").innerHTML = "You've a decided to wait another year to apply. It is May again, and you're starting to prepare for college applications. <br><br> Pamela: " + "So, what's your intended major this time?"
 
-    document.querySelector("#one").innerText = "Sociology"
-    document.querySelector("#one").style.display = "block"
+    document.querySelector("#p").innerText = "Sociology"
+    document.querySelector("#p").style.display = "block"
+    document.querySelector("#p").onclick = p
 
-    document.querySelector("#two").innerText = "Business"
-    document.querySelector("#two").style.display = "block"
+    document.querySelector("#onep").innerText = "Business"
+    document.querySelector("#onep").style.display = "block"
+    document.querySelector("#onep").onclick = onep
 }
 
-function one() {
+function p() {
     // End timer
     clearTimeout(slowTimer)
     sadend.pause()
 
     document.getElementById("p1").innerHTML = "Pamela: *Looks at your transcript* Hmm... ok, then you are going to apply these schools."
 
-    document.querySelector("#one").innerText = "Ask to apply Ivy League"
-    document.querySelector("#one").onclick = deadend
-    document.querySelector("#two").innerText = "Follow Pamela's College List"
-    document.querySelector("#two").onclick = movingon
+    document.querySelector("#onep").innerText = "Ask to apply Ivy League"
+    document.querySelector("#onep").onclick = deadend
+    document.querySelector("#p").innerText = "Follow Pamela's College List"
+    document.querySelector("#p").onclick = movingon
 
 }
 
-function two() {
+function onep() {
     // End timer
     clearTimeout(slowTimer)
 
     sadend.play()
 
     document.querySelector(".container").style.backgroundImage = "URL('end-salaryman.gif')"
-    document.querySelector("#one").style.display = "none"
-    document.querySelector("#two").style.display = "none"
+    document.querySelector("#p").style.display = "none"
+    document.querySelector("#onep").style.display = "none"
     document.getElementById("p1").innerHTML = "Pamela: *Looks at your transcript* Hmm... YOUR GPA DOES NOT EVEN QUALIFY you to GRADUATE, YOU'LL never get into any universities' department with that GPA. You failed her expectation, after highschool, you became a normal salaryman working from 9am to 10pm."
     
     document.querySelector("#res").style.display = "block"
@@ -102,33 +104,33 @@ function deadend() {
 
     document.getElementById("p1").innerHTML = "You did not listen to Pamela, she decides to kick you out of school. You end up going nowhere and became poor and lonely."
     document.querySelector(".container").style.backgroundImage = "URL('end-poor.gif')"
-    document.querySelector("#one").style.display = "none"
-    document.querySelector("#two").style.display = "none"
+    document.querySelector("#start").style.display = "none"
+    document.querySelector("#p").style.display = "none"
+    document.querySelector("#onep").style.display = "none"
 
     document.querySelector("#res").style.display = "block"
 }
 
 function movingon(){
-    document.querySelector("#res").style.display = "block"
-    document.querySelector("#start").style.display = "block"
+    document.querySelector("#res").style.display = "none"
+    document.querySelector("#start").style.display = "none"
 
     sadend.pause()
 
     document.getElementById("p1").innerHTML = "You listened to Pamela, and today is your decision result release date!!"
     document.querySelector(".container").style.backgroundImage = "URL('home.gif')"
-    document.querySelector("#one").innerText = "Ask to apply Ivy League"
-    document.querySelector("#one").onclick = deadend
-    document.querySelector("#two").innerText = "Follow Pamela's College List"
-    document.querySelector("#two").onclick = movingon
-
+    document.querySelector("#p").innerText = "Open the decision letter"
+    document.querySelector("#p").onclick = end
+    document.querySelector("#onep").innerText = "Burn it"
+    document.querySelector("#onep").onclick = deadend
 }
 
 function end(){
 
     happyend.play()
 
-    document.getElementById("p1").innerHTML = "You listened to Pamela, and got into a decent school. You became rich and retired early to enjoy life. You opened a little store in downtown Japan."
+    document.getElementById("p1").innerHTML = "You listened to Pamela, didn't burn your decision letter, and got into a decent school. You became rich and retired early to enjoy life. You opened a little store in downtown Japan."
     document.querySelector(".container").style.backgroundImage = "URL('happyend.gif')"
-    document.querySelector("#one").style.display = "none"
-    document.querySelector("#two").style.display = "none"
+    document.querySelector("#p").style.display = "none"
+    document.querySelector("#onep").style.display = "none"
 }
